97d50070fcb0afcb892e1dad00ae66008d445cd9
Akshsta <akshatafansekar17@gmail.com>
2025-05-21 00:55:45 +0530

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,9 @@
+@Entity
+public class Account {
+    @Id
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+    // add other fields as needed
+    // generate getters and setters
+}
